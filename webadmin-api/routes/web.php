b8a0ddeb2c440a31fb3762d20df659b82c670b84
pablo <pabloripoll.it@gmail.com>
2024-02-22 19:21:11 +0100

    maint: laravel web home route

--- webadmin-api/routes/web.php
+++ webadmin-api/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response()->json(['message' => app()->version()]);
+    return response()->json(['laravel' => app()->version()]);
 });
