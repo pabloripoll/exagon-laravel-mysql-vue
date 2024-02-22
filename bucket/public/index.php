d838e262a043328a6290aa18cd2abdaeedf75213
pablo <pabloripoll.it@gmail.com>
2024-02-22 19:19:41 +0100

    feat: bucket app structure and temp support

--- bucket/public/index.php
+++ bucket/public/index.php
@@ -1,14 +1,5 @@
 <?php
 
-// PHP Info
-$config = false;
-if ($config) {
-    echo phpinfo();
-
-    exit;
-}
-
-// In case of ERROR 500
 $display_errors = true;
 if ($display_errors) {
 	ini_set('display_errors', 1);
@@ -16,59 +7,24 @@ if ($display_errors) {
 	error_reporting(E_ALL);
 }
 
-// PHP PDO ???
-$host = "192.168.1.41:8815";
-$database = "webadmin";
-$username = "webadmin";
-$password = "SuP9r*S3cr3t";
+require_once dirname(__DIR__, 1).'/vendor/autoload.php';
 
-// mysqli Class
-try {
-    $connection = new mysqli($host, $username, $password, $database);
+use App\Request;
+use App\Response;
 
-    // Check connection
-    if ($connection->connect_error) {
-        die("mysql::class() connect failed: " . $connection->connect_error);
-    }
-    echo "mysql::class() connects successfully";
+$request = new Request;
+$response = new Response;
 
-    $connection->close();
+if ($request->method == 'get' && $request->route == '/router/test') {
+    echo '<pre>'; print_r($request); echo '</pre>';
 
-} catch(Exception $e) {
-    echo "mysql::class() connect failed: " . $e->getMessage() . ' - ' . $host;
+    exit;
 }
 
-echo "<br>";
+if ($request->method == 'post') {
+    $response->json($request);
 
-// mysqli Function
-try {
-    $connection = mysqli_connect($host, $username, $password, $database);
-
-    if (! $connection) {
-        die("mysqli_connect() connect failed: " . mysqli_connect_error());
-
-        exit;
-    }
-
-    echo "mysqli_connect() connects successfully";
-
-    mysqli_close($connection);
-
-} catch(Exception $e) {
-    echo "mysqli_connect() connect failed: " . $e->getMessage() . ' - ' . $host;
+    exit;
 }
 
-echo "<br>";
-
-// PDO
-try {
-    $connection = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-
-    // Set PDO error mode to exception
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo "PDO connects successfully";
-
-} catch(\PDOException $e) {
-    echo "PDO connect failed: " . $e->getMessage();
-}
+$response->json(['message' => 'no resource']);
